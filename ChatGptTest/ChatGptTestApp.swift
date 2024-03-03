458b85acc6fd3c7018100a99231546a59ad1d75f
paulshnurenko <paulsnurenko@gmail.com>
2024-03-03 16:30:16 +0100

    Initial Commit

---
+++ ChatGptTest/ChatGptTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGptTestApp.swift
+//  ChatGptTest
+//
+//  Created by Pavlo Shnurenko on 03.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct ChatGptTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
